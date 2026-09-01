bc7a5566624f9705ffafc4d5b4e7e54a1e973cbd
Beki junior <bbekijunior1@gmail.com>
2026-09-01 22:46:51 +0300

    register page is done

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,49 @@
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+  try {
+    const { name, email, password } = await request.json();
+
+    if (!name || !email || !password) {
+      return Response.json({ message: "Fill All fields" }, { status: 400 });
+    }
+
+    const userIsHere = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userIsHere) {
+      return Response.json(
+        { message: "Email already exists" },
+        { status: 400 },
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return Response.json(
+      {
+        message: "Account created successfully",
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error(error);
+
+    return Response.json({ message: "Something went wrong" }, { status: 500 });
+  }
+}
